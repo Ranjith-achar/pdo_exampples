3825280d364953ee34f3960c87829856bc441534
Ranjith-thimmachari <ranjith.thimmachari@gmail.com>
2018-03-12 08:54:04 +0530

    order changed

--- 6_fetch_method3.php
+++ 6_fetch_method3.php
@@ -2,20 +2,6 @@
 
 include 'connect.php';
 
-//foreach($db->query("SELECT * FROM pdo_post_table") as $row ){
-/*
-$stmnt=$db->query("SELECT * FROM pdo_post_table");
-
-while($row=$stmnt->fetch())
-{
-
-//cho $row['titile']." ".$row['body']." ".$row['author']."</br>";
-
-
-echo "<pre>" . var_dump($row) . "</pre>";
-
-}
-*/
 $stmnt=$db->query("SELECT * FROM  pdo_post_table");
 
 while($row=$stmnt->fetch(PDO::FETCH_ASSOC)){
@@ -24,8 +10,6 @@ echo "<pre>" .var_dump($row). "</pre>";
 
 
 }
-
-
 /*
 in fetch method another one method thr to fetch
 
